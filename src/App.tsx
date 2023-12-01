37c81a192d624ea262effb961c185721b657c584
phyothantzin <phyothantzin.w@gmail.com>
2023-12-01 18:14:52 +0630

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import "./globals.css";
+import AuthLayout from "./_auth/AuthLayout";
+import SignIn from "./_auth/Forms/SignIn";
+import SignUp from "./_auth/Forms/SignUp";
+import { Home } from "./_root/pages";
+import RootLayout from "./_root/RootLayout";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Route>
+
+        {/* Private Routes */}
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
